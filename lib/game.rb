41bb45ac1c66d71dd60c18c06b42b29ee1d5de0f
Jacob Moyle <jacobmoyle@gmail.com>
2015-12-02 15:22:28 -0600

    minor refactoring

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,24 @@
 class Game
   def initialize(messages)
     @messages = messages
+    @total_guesses = 10
     start
   end
 
   def start
     @messages.greet
 
-    guesses = 10
-    until guesses == 0
-      @messages.prompt_guess(guesses)
-      user_input
-      guesses -= 1
+    until @total_guesses == 0
+      player_turn
+      @total_guesses -= 1
     end
 
     @messages.goodbye
+  end
+
+  def player_turn
+    @messages.prompt_guess(@total_guesses)
+    user_input
   end
 
   def user_input
