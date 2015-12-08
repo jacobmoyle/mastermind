9d7662cd428fcd3886f67706fe907eedc1b7c3e8
Jacob Moyle <jacobmoyle@gmail.com>
2015-12-08 16:17:34 -0600

    refactored tests, created a helper method

--- spec/guess_valitator_spec.rb
+++ spec/guess_valitator_spec.rb
@@ -28,37 +28,41 @@ describe GuessValidator do
     end
 
     it 'can accept uppercase or lowercase input' do
-      expect(subject.validate('a234').feedback).to eq('o')
-      expect(subject.validate('A234').feedback).to eq('o')
+      expect(feedback('a234')).to eq('o')
+      expect(feedback('A234')).to eq('o')
     end
 
     context 'when guess is completely correct' do
       it 'responds with true' do
-        expect(subject.validate('ABCD').feedback).to eq('oooo')
+        expect(feedback('ABCD')).to eq('oooo')
       end
     end
 
     context 'when guess is incorrect' do
 
       it 'responds with false if completely incorrect' do
-        expect(subject.validate('1234').feedback).to eq('')
+        expect(feedback('1234')).to eq('')
       end
 
       it 'provides o for any correct character in the correct spot' do
-        expect(subject.validate('A234').feedback).to eq('o')
-        expect(subject.validate('1B34').feedback).to eq('o')
-        expect(subject.validate('12C4').feedback).to eq('o')
-        expect(subject.validate('123D').feedback).to eq('o')
+        expect(feedback('A234')).to eq('o')
+        expect(feedback('1B34')).to eq('o')
+        expect(feedback('12C4')).to eq('o')
+        expect(feedback('123D')).to eq('o')
       end
 
       # No tests written to cover duplicate characters
       it 'provides x if a character is correct but in the incorrect spot' do
-        expect(subject.validate('1A34').feedback).to eq('x')
-        expect(subject.validate('B234').feedback).to eq('x')
-        expect(subject.validate('123C').feedback).to eq('x')
-        expect(subject.validate('1D34').feedback).to eq('x')
+        expect(feedback('1A34')).to eq('x')
+        expect(feedback('B234')).to eq('x')
+        expect(feedback('123C')).to eq('x')
+        expect(feedback('1D34')).to eq('x')
       end
     end
   end
+end
 
+# Helper Methods
+def feedback(guess)
+  subject.validate(guess).feedback
 end
