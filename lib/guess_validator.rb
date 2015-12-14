caecd0f0a95608ad3291e0aa9d879343e0affaf9
Jacob Moyle <jacobmoyle@gmail.com>
2015-12-13 19:39:12 -0600

    refactored public facing method and feedback creation organization

--- lib/guess_validator.rb
+++ lib/guess_validator.rb
@@ -5,44 +5,48 @@ class GuessValidator
     @exact_match_char = 'o'
     @contains_match_char = 'x'
     @response = ''
+    @error = 'Code length is incorrect, code is 4 characters'
   end
 
   def validate(unsolved_code, player_code)
     set_instance_variables(unsolved_code, player_code)
     @response = ''
 
-    if @formatted_guess.length != @unsolved_code.length
-      'Code length is incorrect, code is 4 characters'
-    else
-      provide_feedback
-    end
+    return @error if @formatted_guess.length != @unsolved_code.length
+
+    create_feedback
+    @response
   end
 
   private
 
-  def provide_feedback
+  def create_feedback
     @formatted_guess.each_with_index do |guess, guess_index|
+      exact_feedback(guess_index) if perfect_match?(guess, guess_index)
 
-      exact_feedback_for(guess_index) if perfect_match?(guess, guess_index)
+      target_index = @unsolved_code.index(guess)
 
-      if unused_character?(guess) && target_index = @unsolved_code.index(guess)
-        concat_feedback(@contains_match_char)
-
-        mark_as_used(@unsolved_code, target_index)
-        mark_as_used(@formatted_guess, guess_index)
-      end
+      relative_feedback(guess_index, target_index) if relative_match?(guess, target_index)
     end
-
-    @response
   end
 
+  def relative_match?(guess_char, target)
+    unused_character?(guess_char) && target != nil
+  end
+
+  def relative_feedback(guess_index, target_index)
+    add_character_feedback(@contains_match_char)
+
+    mark_as_used(@unsolved_code, target_index)
+    mark_as_used(@formatted_guess, guess_index)
+  end
 
   def perfect_match?(guess_char, guess_index)
     guess_char == @unsolved_code[guess_index] && unused_character?(guess_char)
   end
 
-  def exact_feedback_for(index)
-    concat_feedback(@exact_match_char)
+  def exact_feedback(index)
+    add_character_feedback(@exact_match_char)
 
     mark_as_used(@unsolved_code, index)
     mark_as_used(@formatted_guess, index)
@@ -54,7 +58,7 @@ class GuessValidator
     @response = ''
   end
 
-  def concat_feedback(feedback)
+  def add_character_feedback(feedback)
     @response.concat(feedback)
   end
 
