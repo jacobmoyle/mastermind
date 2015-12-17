4c20fb16f5c36f5b60e32f2e6ae9964628c40066
Jacob Moyle <jacobmoyle@gmail.com>
2015-12-17 12:29:51 -0600

    completed set_hidden_code, updated player_guess test to utilize the code generator, rather than hard coding

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,25 +3,25 @@ require_relative '../lib/guess_validator'
 require_relative '../lib/code_generator'
 
 describe Game do
-  let(:player)    { double('input') }
+  let(:player)     { double('input') }
   let(:code_maker) { CodeGenerator.new }
-  let(:validator) { GuessValidator.new }
-  let(:game)      { Game.new(
-    input: player,
-    code_checker: validator,
-    code_maker: code_maker,) }
+  let(:validator)  { GuessValidator.new }
+  let(:game)       { Game.new( input: player, code_checker: validator,
+   code_maker: code_maker,) }
 
   describe '#player_guess' do
     it 'asks a player for input until the input is valid' do
-      allow(player).to receive(:guess).and_return('b','','aaaaa','abcd')
+      allow(player).to receive(:guess).and_return( '',
+        code_maker.generate.concat(code_maker.generate), code_maker.generate)
 
-      expect(game.player_guess.length).to be == 4
+      expect(game.player_guess.length).to eq(code_maker.generate.length)
     end
   end
 
   describe '#set_hidden_code' do
     it 'updates the hidden code' do
       previous_code = game.hidden_code
+
       game.new_hidden_code
       expect(game.hidden_code).to_not eq(previous_code)
     end
@@ -30,7 +30,8 @@ describe Game do
   xit 'outputs correct feedback for the players input' do
     # allow(player).to receive(:guess).and_return(code_maker.generate)
 
-    # expected_response = validator.validate(code_maker.generate, player.guess)
+    # expected_response = validator.validate(
+      # code_maker.generate, player.guess)
 
     # expect(game.feedback).to eq(expected_response)
   end
