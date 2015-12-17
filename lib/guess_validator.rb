e1ffb021dbc863b6d90296d56cb310878201599f
Jacob Moyle <jacobmoyle@gmail.com>
2015-12-17 16:46:17 -0600

    minor refactoring for legibility

--- lib/guess_validator.rb
+++ lib/guess_validator.rb
@@ -14,29 +14,36 @@ class GuessValidator
 
   private
 
-  def validation
-    code_array = format_code(@code)
-    guess_array = format_code(@guess)
-    response = ''
-
-    guess_array.each_with_index do |char, index|
-      if char == code_array[index] && char != ' '
-        response.concat('o')
-        code_array[index] = ' '
-        guess_array[index] = ' '
-      end
-
-      if char != ' ' && code_index = code_array.index(char)
-        response.concat('x')
-        code_array[code_index] = ' '
-        guess_array[index] = ' '
-      end
-    end
-
-    response
+  def used_character
+    nil
   end
 
   def format_code(string)
     string.upcase.split('')
+  end
+
+  def validation
+    code_array  = format_code(@code)
+    guess_array = format_code(@guess)
+    response    = String.new
+
+    guess_array.each_with_index do |char, index|
+
+      if char == code_array[index] && char != used_character
+
+        response.concat('o')
+        code_array[index]  = used_character
+        guess_array[index] = used_character
+      end
+
+      if char != used_character && code_index = code_array.index(char)
+
+        response.concat('x')
+        code_array[code_index] = used_character
+        guess_array[index]     = used_character
+      end
+    end
+
+    response
   end
 end
