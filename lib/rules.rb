908f7dc6864f6f5cb66c238a5760846a04ea7078
Jacob Moyle <jacobmoyle@gmail.com>
2015-12-28 15:55:28 -0600

    tested valid_guess using invalid characters

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,6 +1,18 @@
 class Rules
-  def valid_guess?(guess)
-    return true if guess.length == 4
+  def valid_guess?(string)
+    guess = format(string)
+    valid_char = ['a','b','c','d','e','f']
+
+    return true if guess.length == 4 &&
+    guess.drop_while { |char|
+      valid_char.include?(char)
+      }.empty?
     false
+  end
+
+  private
+
+  def format(string)
+    string.split('')
   end
 end
--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -1,22 +1,29 @@
 require_relative '../lib/rules'
 
 describe Rules do
+
   describe '#valid_guess?' do
-    it 'returns true if the input is 4 characters long' do
-      expect(subject.valid_guess?('cccc')).to eq(true)
+
+    context 'when using correct characters' do
+
+      it 'returns true if the input is 4 characters long' do
+        expect(subject.valid_guess?('cccc')).to eq(true)
+      end
+      it 'it returns false if the input is greater than 4' do
+        expect(subject.valid_guess?('caaac')).to eq(false)
+      end
+      it 'returns false if the input is less than 4' do
+        expect(subject.valid_guess?('cac')).to eq(false)
+      end
     end
-    it 'it returns false if the input is greater than 4' do
-      expect(subject.valid_guess?('caaac')).to eq(false)
-    end
-    it 'returns false if the input is less than 4' do
-      expect(subject.valid_guess?('cac')).to eq(false)
-    end
-    it 'returns true if the input contains a' do
-      expect(subject.valid_guess?('caaac')).to eq(false)
-    end
-    xit 'b, etc.' do
-    end
-    xit 'returns false if the input contains anything outside of the specified characters above (flush this description out)' do
+
+    context 'when using incorrect characters' do
+
+      it 'returns false if the input includes invalid characters at the length of 4' do
+        expect(subject.valid_guess?('¬˚˜å')).to eq(false)
+        expect(subject.valid_guess?('abqr')).to eq(false)
+        expect(subject.valid_guess?('    ')).to eq(false)
+      end
     end
   end
 end
