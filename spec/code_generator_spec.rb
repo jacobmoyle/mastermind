295bee1b729e78a8fb521608762b2701ebed4ba7
Jacob Moyle <jacobmoyle@gmail.com>
2016-01-11 14:55:45 -0600

    updated patterns to be more succinct

--- spec/code_generator_spec.rb
+++ spec/code_generator_spec.rb
@@ -14,10 +14,7 @@ describe CodeGenerator do
     end
 
     it 'returns unique patterns 75% of the time' do
-      patterns = []
-      500.times do
-        patterns << subject.generate
-      end
+      patterns = patterns = 500.times.map{ subject.generate }
       expect(patterns.uniq.count / (patterns.count).to_f).to be >= 0.75
     end
   end
